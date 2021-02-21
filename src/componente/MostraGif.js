c8e40deb43579929f2880c9aa7632c1353208803
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-02-21 12:19:23 -0300

    Delete src directory

--- src/componente/MostraGif.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-function mostraGif (gif){
-    return(
-<img src="https://cdn.dicionariopopular.com/imagens/nazareconfusamatematica.gif" alt="Gif"/>
-    )
-}
-export default mostraGif
