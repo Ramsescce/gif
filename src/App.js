c4564f89dcfca2f24952889578557e1d49a2a157
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-02-21 12:21:10 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import MostraGif from './components/MostraGif'
+function App() {
+  return (
+    <div className="App">
+  <MostraGif/>
+    </div>
+  );
+}
+
+export default App;
