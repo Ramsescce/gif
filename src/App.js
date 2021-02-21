f77dab5e071cdcdb83be1c4754528a87605cf676
Ramses Godoy <74273214+Ramsescce@users.noreply.github.com>
2021-02-21 12:18:38 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import React, { useState } from 'react'
-import MostraGif from './componente/MostraGif'
-
+import React from 'react'
+import MostraGif from './components/MostraGif'
 function App() {
-  const [contador, mudarImg] = useState(0)
-
-  const gif = () => {
-    mudarImg(contador + 1)
-  }
-
   return (
     <div className="App">
-      <button onClick={gif}><b>Clique Aqui!</b> </button>
-      {contador > 0 ?
-        <MostraGif /> :
-        <p></p>
-      }
-    </div >
+  <MostraGif/>
+    </div>
   );
 }
 
